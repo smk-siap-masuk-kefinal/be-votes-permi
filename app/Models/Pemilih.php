d55fd14e17d1463ce3d0e4ab71832039e8d3638d
Obiwannn11 <118538946+Obiwannn11@users.noreply.github.com>
2025-06-20 21:29:36 +0800

    membuat config untuk laravel sanctumn

--- app/Models/Pemilih.php
+++ app/Models/Pemilih.php
@@ -4,10 +4,11 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Laravel\Sanctum\HasApiTokens;
 
 class Pemilih extends Model
 {
-    use HasFactory;
+    use HasFactory, HasApiTokens;
 
     protected $fillable = [
         'nik',
