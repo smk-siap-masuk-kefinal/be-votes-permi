5a26aedef889f37af44210de7d9e2d62c73f2da8
Obiwannn11 <118538946+Obiwannn11@users.noreply.github.com>
2025-06-20 19:10:02 +0800

    membuat fillable

--- app/Models/Pemilih.php
+++ app/Models/Pemilih.php
@@ -3,8 +3,19 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Pemilih extends Model
 {
-    //
+    use HasFactory;
+
+    protected $fillable = [
+        'nik',
+        'nama',
+        'qr_code',
+        'kode_logout',
+        'is_voted',
+        'tps',
+    ];
+
 }
