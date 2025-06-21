ef8a46ab5eca6f103df7e948ccb2196c86c57828
Obiwannn11 <118538946+Obiwannn11@users.noreply.github.com>
2025-06-21 15:01:31 +0800

    final

--- resources/views/landing-page/index.blade.php
+++ resources/views/landing-page/index.blade.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="utf-8" />
     <meta http-equiv="x-ua-compatible" content="ie=edge" />
-    <title>Appvilla - Creative Landing Page HTML Template.</title>
+    <title>VOTES - Valid Online Transparent Election System</title>
     <meta name="description" content="" />
     <meta name="viewport" content="width=device-width, initial-scale=1" />
     <link rel="shortcut icon" type="image/x-icon" href="assets/images/favicon.svg" />
@@ -44,9 +44,9 @@
                     <div class="nav-inner">
                         <!-- Start Navbar -->
                         <nav class="navbar navbar-expand-lg">
-                            <a class="navbar-brand" href="{{ route('landing-page.index') }}">
+                            {{-- <a class="navbar-brand" href="{{ route('landing-page.index') }}">
                                 <img src="{{ asset('images/logo/white-logo.svg') }}" alt="laaogo" />
-                            </a>
+                            </a> --}}
                             <button class="navbar-toggler mobile-menu-btn" type="button" data-bs-toggle="collapse"
                                 data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
                                 aria-expanded="false" aria-label="Toggle navigation">
@@ -122,17 +122,17 @@
                         <p class="wow fadeInLeft" data-wow-delay=".6s">
                             Valid Online Transparent Election System (VOTES) is Voting Platform to solve Election System Problems
                         </p>
-                        <div class="button wow fadeInLeft" data-wow-delay=".8s">
+                        {{-- <div class="button wow fadeInLeft" data-wow-delay=".8s">
                             <a href="javascript:void(0)" class="btn"><i class="lni lni-apple"></i> App Store</a>
                             <a href="javascript:void(0)" class="btn btn-alt"><i class="lni lni-play-store"></i> Google
                                 Play</a>
-                        </div>
+                        </div> --}}
                     </div>
                 </div>
                 <div class="col-lg-7 col-md-12 col-12">
-                    <div class="hero-image wow fadeInRight" data-wow-delay=".4s">
+                    {{-- <div class="hero-image wow fadeInRight" data-wow-delay=".4s">
                         <img src="{{ asset('images/hero/phone.png') }}" alt="#" />
-                    </div>
+                    </div> --}}
                 </div>
             </div>
         </div>
@@ -162,8 +162,6 @@
                         <i class="lni lni-cloud-upload"></i>
                         <h3>Terintegrasi</h3>
                         <p>
-                            It is a long established fact that a reader will be distracted
-                            by the readable content of a page at its layout.
                         </p>
                     </div>
                     <!-- End Single Feature -->
@@ -174,8 +172,6 @@
                         <i class="lni lni-lock"></i>
                         <h3>Keamanan Berlapis</h3>
                         <p>
-                            It is a long established fact that a reader will be distracted
-                            by the readable content of a page at its layout.
                         </p>
                     </div>
                     <!-- End Single Feature -->
@@ -186,8 +182,6 @@
                         <i class="lni lni-reload"></i>
                         <h3>Mempercepat Proses Perhitungan</h3>
                         <p>
-                            It is a long established fact that a reader will be distracted
-                            by the readable content of a page at its layout.
                         </p>
                     </div>
                     <!-- End Single Feature -->
@@ -198,8 +192,6 @@
                         <i class="lni lni-shield"></i>
                         <h3>Perlindungan Data Pengguna</h3>
                         <p>
-                            It is a long established fact that a reader will be distracted
-                            by the readable content of a page at its layout.
                         </p>
                     </div>
                     <!-- End Single Feature -->
@@ -210,8 +202,6 @@
                         <i class="lni lni-cog"></i>
                         <h3>Kemudahan Pengaturan</h3>
                         <p>
-                            It is a long established fact that a reader will be distracted
-                            by the readable content of a page at its layout.
                         </p>
                     </div>
                     <!-- End Single Feature -->
@@ -222,8 +212,6 @@
                         <i class="lni lni-layers"></i>
                         <h3></h3>
                         <p>
-                            It is a long established fact that a reader will be distracted
-                            by the readable content of a page at its layout.
                         </p>
                     </div>
                     <!-- End Single Feature -->
@@ -489,121 +477,7 @@
     <!-- End Call To Action Area -->
 
     <!-- Start Footer Area -->
-    <footer class="footer">
-        <!-- Start Footer Top -->
-        <div class="footer-top">
-            <div class="container">
-                <div class="row">
-                    <div class="col-lg-4 col-md-4 col-12">
-                        <!-- Single Widget -->
-                        <div class="single-footer f-about">
-                            <div class="logo">
-                                <a href="index.html">
-                                    <img src="{{ asset('images/logo/white-logo.svg') }}" alt="#" />
-                                </a>
-                            </div>
-                            <p>
-                                Making the world a better place through constructing elegant
-                                hierarchies.
-                            </p>
-                            <ul class="social">
-                                <li>
-                                    <a href="javascript:void(0)"><i class="lni lni-facebook-filled"></i></a>
-                                </li>
-                                <li>
-                                    <a href="javascript:void(0)"><i class="lni lni-twitter-original"></i></a>
-                                </li>
-                                <li>
-                                    <a href="javascript:void(0)"><i class="lni lni-instagram"></i></a>
-                                </li>
-                                <li>
-                                    <a href="javascript:void(0)"><i class="lni lni-linkedin-original"></i></a>
-                                </li>
-                                <li>
-                                    <a href="javascript:void(0)"><i class="lni lni-youtube"></i></a>
-                                </li>
-                                <li>
-                                    <a href="javascript:void(0)"><i class="lni lni-pinterest"></i></a>
-                                </li>
-                            </ul>
-                            <p class="copyright-text">
-                                Designed and Developed by
-                                <a href="https://uideck.com/" rel="nofollow" target="_blank">UIdeck</a>
-                            </p>
-                        </div>
-                        <!-- End Single Widget -->
-                    </div>
-                    <div class="col-lg-8 col-md-8 col-12">
-                        <div class="row">
-                            <div class="col-lg-3 col-md-6 col-12">
-                                <!-- Single Widget -->
-                                <div class="single-footer f-link">
-                                    <h3>Solutions</h3>
-                                    <ul>
-                                        <li><a href="javascript:void(0)">Marketing</a></li>
-                                        <li><a href="javascript:void(0)">Analytics</a></li>
-                                        <li><a href="javascript:void(0)">Commerce</a></li>
-                                        <li><a href="javascript:void(0)">Insights</a></li>
-                                        <li><a href="javascript:void(0)">Promotion</a></li>
-                                    </ul>
-                                </div>
-                                <!-- End Single Widget -->
-                            </div>
-                            <div class="col-lg-3 col-md-6 col-12">
-                                <!-- Single Widget -->
-                                <div class="single-footer f-link">
-                                    <h3>Support</h3>
-                                    <ul>
-                                        <li><a href="javascript:void(0)">Pricing</a></li>
-                                        <li><a href="javascript:void(0)">Documentation</a></li>
-                                        <li><a href="javascript:void(0)">Guides</a></li>
-                                        <li><a href="javascript:void(0)">API Status</a></li>
-                                        <li><a href="javascript:void(0)">Live Support</a></li>
-                                    </ul>
-                                </div>
-                                <!-- End Single Widget -->
-                            </div>
-                            <div class="col-lg-3 col-md-6 col-12">
-                                <!-- Single Widget -->
-                                <div class="single-footer f-link">
-                                    <h3>Company</h3>
-                                    <ul>
-                                        <li><a href="javascript:void(0)">About Us</a></li>
-                                        <li><a href="javascript:void(0)">Our Blog</a></li>
-                                        <li><a href="javascript:void(0)">Jobs</a></li>
-                                        <li><a href="javascript:void(0)">Press</a></li>
-                                        <li><a href="javascript:void(0)">Contact Us</a></li>
-                                    </ul>
-                                </div>
-                                <!-- End Single Widget -->
-                            </div>
-                            <div class="col-lg-3 col-md-6 col-12">
-                                <!-- Single Widget -->
-                                <div class="single-footer f-link">
-                                    <h3>Legal</h3>
-                                    <ul>
-                                        <li>
-                                            <a href="javascript:void(0)">Terms & Conditions</a>
-                                        </li>
-                                        <li><a href="javascript:void(0)">Privacy Policy</a></li>
-                                        <li>
-                                            <a href="javascript:void(0)">Catering Services</a>
-                                        </li>
-                                        <li>
-                                            <a href="javascript:void(0)">Customer Relations</a>
-                                        </li>
-                                        <li><a href="javascript:void(0)">Innovation</a></li>
-                                    </ul>
-                                </div>
-                                <!-- End Single Widget -->
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-        <!--/ End Footer Top -->
-    </footer>
+   
     <!--/ End Footer Area -->
 
     <!-- ========================= scroll-top ========================= -->
